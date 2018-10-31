aa910c249331c101df156e879547b3cc6c522883
invchar <16106734+invchar@users.noreply.github.com>
2018-10-31 13:19:16 -0500

    New functions added

--- screenlymanagement.py
+++ screenlymanagement.py
@@ -1,17 +1,20 @@
 import requests
 import sys
 import os
+import json
 
 HOST = "https://api.screenlyapp.com"
-API_ROOT = "/api/v3"
+API_ROOT = "api/v3"
 API_KEY = os.getenv('API_KEY')
 
 session = requests.Session()
 
-session.headers["Authorization"] = "Token {}".format(API_KEY)
+session.headers = {"Authorization":"Token {}".format(API_KEY),"content-type":"application/json"}
 
-def enablePlaylist(playlistID, enabled):
-	r = session.patch((HOST + API_ROOT + "/playlists/{}/".format(playlistID)),({"is_enabled":enabled}))
+def enablePlaylists(playlistIds, enabled):
+	for playlistId in playlistIds:
+		session.patch((HOST + API_ROOT + "/playlists/{}/".format(playlistId)),({"is_enabled":enabled}))
+	return 0
 
 def getPlaylistIds():
 	r = session.get((HOST + API_ROOT + "/playlists"))
@@ -21,8 +24,8 @@ def getPlaylistIds():
 		playlistIds.append(playlist["id"])
 	return playlistIds
 
-def getPlaylistAssetIds(playlistID):
-	r = session.get((HOST + API_ROOT + "/playlists/{}".format(playlistID)))
+def getPlaylistAssetIds(playlistId):
+	r = session.get((HOST + API_ROOT + "/playlists/{}".format(playlistId)))
 	playlist = r.json()
 	assets = playlist["assets"]
 	assetIds = []
@@ -30,11 +33,31 @@ def getPlaylistAssetIds(playlistID):
 		assetIds.append(asset["_id"])
 	return assetIds
 
-#def replaceAsset(oldAssetId, newAssetId):
-#	playlistIds = getPlaylistIds()
-#	masterList = {}
-#	for playlistId in playlistIds:
-#		masterList.update({playlistId:getPlaylistAssetIds(playlistId)})
-#	for playlistId, assets in masterList.items():
-#		if oldAssetId in assets:
+def getPlaylistAssets(playlistId):
+	r = session.get('{}/{}/playlists/{}/'.format(HOST,API_ROOT,playlistId))
+	playlist = r.json()
+	return playlist["assets"]
 
+def removeAsset(playlistIds,assetId):
+	for playlistId in playlistIds:
+		originalAssets = getPlaylistAssets(playlistId)
+		newAssets = []
+		for asset in originalAssets:
+			if (asset["_id"] != assetId):
+				asset["id"] = asset["_id"]
+				newAssets.append(asset)
+		session.patch('{}/{}/playlists/{}/'.format(HOST,API_ROOT,playlistId),data = json.dumps({"assets":newAssets}))
+	return 0
+
+def replaceAsset(playlistIds,oldAssetId,newAssetId,duration):
+	for playlistId in playlistIds:
+		originalAssets = getPlaylistAssets(playlistId)
+		newAssets = []
+		for asset in originalAssets:
+			if (asset["_id"] == oldAssetId):
+				newAssets.append({"id":newAssetId,"duration":duration})
+			else:
+				asset["id"] = asset["_id"]
+				newAssets.append(asset)
+		session.patch('{}/{}/playlists/{}/'.format(HOST,API_ROOT,playlistId),data = json.dumps({"assets":newAssets}))
+	return 0
