54a54c13033ea23b48f49a19e037b9b4b0cb8904
invchar <16106734+invchar@users.noreply.github.com>
2018-11-02 17:34:42 -0500

    Begin adding arguments processing

--- screenlymanagement.py
+++ screenlymanagement.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import os
 import json
+import argparse
 
 HOST = "https://api.screenlyapp.com"
 API_ROOT = "api/v3"
@@ -11,13 +12,24 @@ session = requests.Session()
 
 session.headers = {"Authorization":"Token {}".format(API_KEY),"content-type":"application/json"}
 
+parser = argparse.ArgumentParser(description='Manage Screenly via API')
+parser.add_argument('--action', choices=['enablePlaylists','removeAsset','replaceAsset'])
+parser.add_argument('--playlistIds')
+parser.add_argument('--assetId')
+parser.add_argument('--oldAssetId')
+parser.add_argument('--newAssetId')
+parser.add_argument('--duration')
+args = parser.parse_args()
+
 def enablePlaylists(playlistIds, enabled):
+	if ('ALL' in playlistIds):
+		playlistIds = getPlaylistIds()
 	for playlistId in playlistIds:
 		session.patch((HOST + API_ROOT + "/playlists/{}/".format(playlistId)),({"is_enabled":enabled}))
 	return 0
 
 def getPlaylistIds():
-	r = session.get((HOST + API_ROOT + "/playlists"))
+	r = session.get('{}/{}/playlists/'.format(HOST,API_ROOT))
 	playlists = r.json()
 	playlistIds = []
 	for playlist in playlists:
@@ -65,3 +77,11 @@ def replaceAsset(playlistIds,oldAssetId,newAssetId,duration):
 				newAssets.append(asset)
 		session.patch('{}/{}/playlists/{}/'.format(HOST,API_ROOT,playlistId),data = json.dumps({"assets":newAssets}))
 	return 0
+
+if (vars(args)['action']):
+	if (vars(args)['action'] == 'enablePlaylists'):
+		enablePlaylists(vars(args)['playlistIds'],True)
+	if (vars(args)['action'] == 'removeAsset'):
+		removeAsset(vars(args)['playlistIds'],vars(args)['assetId'])
+	if (vars(args)['action'] == 'replaceAsset'):
+		replaceAsset(vars(args)['playlistIds'],vars(args)['oldAssetId'],vars(args)['newAssetId'],vars(args)['duration'])
